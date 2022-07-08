df5adbe3f62f02db22baa2c006fc7c38d3f4d284
Ansh <ANSHSINGH25BD@GMAIL.COM>
2022-07-09 00:26:38 +0530

    Memory management

--- common/service.sh
+++ common/service.sh
@@ -5,3 +5,23 @@ MODDIR=${0%/*}
 
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
+
+# Virtual memory tweaks
+stop perfd
+echo '80' > /proc/sys/vm/swappiness
+echo '0' > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
+echo '100' > /proc/sys/vm/vfs_cache_pressure
+echo '128' > /sys/block/mmcblk0/queue/read_ahead_kb
+echo '128' > /sys/block/mmcblk1/queue/read_ahead_kb
+echo '8000' > /proc/sys/vm/min_free_kbytes
+echo '0' > /proc/sys/vm/oom_kill_allocating_task
+echo '5' > /proc/sys/vm/dirty_ratio
+echo '20' > /proc/sys/vm/dirty_background_ratio
+chmod 666 /sys/module/lowmemorykiller/parameters/minfree
+chown root /sys/module/lowmemorykiller/parameters/minfree
+echo '21816,29088,36360,43632,50904,65448' > /sys/module/lowmemorykiller/parameters/minfree
+rm /data/system/perfd/default_values
+start perfd
+sleep 20
+# Set Activity Manager's max. cached app number -> 160 (instead of the default 32 (or even lower 24):
+settings put global activity_manager_constants max_cached_processes=160
